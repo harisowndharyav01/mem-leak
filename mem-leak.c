6a1a2322290e283c3d0f829ab3cd00fc22f1d5c7
Harisowndharya V <147607204+harisowndharyav01@users.noreply.github.com>
2025-09-09 09:39:01 +0530

    Update mem-leak.c

--- mem-leak.c
+++ mem-leak.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main() {
     while (1) {
-        malloc(1024 * 1024);  // leak 1MB each second
+        void *p = malloc(1024 * 1024); // leak 1 MB
+        printf("Leaked 1 MB at %p\n", p);
         sleep(1);
     }
     return 0;
